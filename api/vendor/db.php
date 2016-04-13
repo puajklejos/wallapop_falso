3f871db209ac74ff476f009369bbb0b1a3ff0fc4
Martí Gómez <magopru1995@gmail.com>
2016-04-13 19:14:23 +0200

    NuSoap

---
+++ api/vendor/db.php
@@ -0,0 +1,12 @@
+<?php
+	
+	/*Conexion a la base de datos*/
+	function getDB()
+	{
+		$usuario = "wallapop";
+		$contraseña = "1234";
+		$mbd = new PDO('mysql:host=localhost;dbname=wallapop', $usuario, $contraseña);
+	}
+
+
+?>
